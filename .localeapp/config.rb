a8cd203b4ba8543de33959aa2eed0a941d5ad276
Thomas von Deyen <thomas@vondeyen.com>
2021-10-26 11:48:40 +0200

    Update localeapp config
    The data directory has changed

--- .localeapp/config.rb
+++ .localeapp/config.rb
@@ -2,7 +2,7 @@ require 'dotenv/load'
 
 Localeapp.configure do |config|
   config.api_key                    = ENV['LOCALEAPP_API_KEY']
-  config.translation_data_directory = 'config/locales'
+  config.translation_data_directory = 'locales'
   config.synchronization_data_file  = '.localeapp/log.yml'
   config.daemon_pid_file            = '.localeapp/localeapp.pid'
 end
