770f06c76aa47f73d69382b2ececb5242f1bfeb0
Thomas von Deyen <thomas@vondeyen.com>
2022-04-17 22:51:00 +0200

    Append locales to I18n load path
    Either all or only the ones defined by
    the Rails apps available_locales config.

--- lib/alchemy_i18n/engine.rb
+++ lib/alchemy_i18n/engine.rb
@@ -1,4 +1,10 @@
 module AlchemyI18n
   class Engine < ::Rails::Engine
+    initializer "alchemy-i18n" do |app|
+      locales = Array(app.config.i18n.available_locales)
+      pattern = locales.empty? ? "*" : "{#{locales.join ","}}"
+      files = Dir[root.join("locales", "alchemy.#{pattern}.yml")]
+      I18n.load_path.concat(files)
+    end
   end
 end
