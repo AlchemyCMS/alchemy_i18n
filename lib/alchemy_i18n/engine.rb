86d0d0f09bb0125f8e94606ec6529bbf6b91fa43
Thomas von Deyen <thomas@vondeyen.com>
2023-10-04 15:17:13 +0200

    Import flatpickr translations into Alchemy admin
    Alchemy 7.0 uses importmaps and JS modules for
    flatpickr (and other JS dependencies). We need
    to register the translations as additional JS
    module imports in order to be available to
    Flatpickr.

--- lib/alchemy_i18n/engine.rb
+++ lib/alchemy_i18n/engine.rb
@@ -5,6 +5,10 @@ module AlchemyI18n
       pattern = locales.empty? ? "*" : "{#{locales.join ","}}"
       files = Dir[root.join("locales", "alchemy.#{pattern}.yml")]
       I18n.load_path.concat(files)
+      locales.each do |locale|
+        Alchemy.importmap.pin "flatpickr/#{locale}.js", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/l10n/#{locale}.js"
+        Alchemy.admin_js_imports << "flatpickr/#{locale}.js"
+      end
     end
   end
 end
