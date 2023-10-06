c6ba4fde28a739b7c07ab1dc8a0c1175451be623
Thomas von Deyen <thomas@vondeyen.com>
2023-10-06 11:00:52 +0200

    Do not attempt to load en flatpickr locale
    It does not exist and projects might still allow
    en locale

--- lib/alchemy_i18n/engine.rb
+++ lib/alchemy_i18n/engine.rb
@@ -1,7 +1,7 @@
 module AlchemyI18n
   class Engine < ::Rails::Engine
     initializer "alchemy-i18n" do |app|
-      locales = Array(app.config.i18n.available_locales)
+      locales = Array(app.config.i18n.available_locales).reject { |l| l == :en }
       pattern = locales.empty? ? "*" : "{#{locales.join ","}}"
       files = Dir[root.join("locales", "alchemy.#{pattern}.yml")]
       I18n.load_path.concat(files)
