79ecde94b8025650fe20d1face760176f1815ea5
Thomas von Deyen <thomas@vondeyen.com>
2024-06-20 10:23:22 +0200

    Download flatpickr locales
    Serve flatpickr locales from this gem via Sprockets instead
    of via the CDN.

--- lib/alchemy_i18n/engine.rb
+++ lib/alchemy_i18n/engine.rb
@@ -8,7 +8,7 @@ module AlchemyI18n
       app.config.assets.paths << AlchemyI18n::Engine.root.join("vendor/locales")
       config.assets.precompile << "alchemy_i18n.js"
       locales.each do |locale|
-        Alchemy.importmap.pin "flatpickr/#{locale}.js", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/l10n/#{locale}.js"
+        Alchemy.importmap.pin "flatpickr/#{locale}.js", to: "flatpickr/#{locale}.js"
         Alchemy.importmap.pin "select2/#{locale}.js", to: "select2_locale_#{locale}.js"
         Alchemy.admin_js_imports << "flatpickr/#{locale}.js"
         Alchemy.admin_js_imports << "select2/#{locale}.js"
