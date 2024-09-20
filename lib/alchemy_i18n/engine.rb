fc1a687bd42ba75303b55a6d878d72d55d8a74fc
Thomas von Deyen <thomas@vondeyen.com>
2024-09-20 21:52:49 +0200

    Load select2 locales via importmap
    Alchemy now loads select2 as node module via an importmap,
    so we need to load it's locales the same way.

--- lib/alchemy_i18n/engine.rb
+++ lib/alchemy_i18n/engine.rb
@@ -5,9 +5,13 @@ module AlchemyI18n
       pattern = locales.empty? ? "*" : "{#{locales.join ","}}"
       files = Dir[root.join("locales", "alchemy.#{pattern}.yml")]
       I18n.load_path.concat(files)
+      app.config.assets.paths << AlchemyI18n::Engine.root.join("vendor/locales")
+      config.assets.precompile << "alchemy_i18n.js"
       locales.each do |locale|
         Alchemy.importmap.pin "flatpickr/#{locale}.js", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/l10n/#{locale}.js"
+        Alchemy.importmap.pin "select2/#{locale}.js", to: "select2_locale_#{locale}.js"
         Alchemy.admin_js_imports << "flatpickr/#{locale}.js"
+        Alchemy.admin_js_imports << "select2/#{locale}.js"
       end
     end
   end
